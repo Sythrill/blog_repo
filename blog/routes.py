ba23e10615f102393278e88cb929b4c0d131e4a5
Katarzyna Sciwiarska <ksciwiarska@gmail.com>
2020-06-23 20:52:53 +0200

    projekt

---
+++ blog/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+from blog.models import Entry
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
